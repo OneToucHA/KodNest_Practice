4c8ca54cfc03f5a834dbae485d9ccb5cc93db2bd
Amogh Angadi <amoghanj23@gmail.com>
2026-08-04 11:47:20 +0530

    Kod_practice

---
+++ check positive or negative.py
@@ -0,0 +1,8 @@
+# 1. write a program to check the number is positive or negative 
+a=int(input("enter a number :"))
+if a>0:
+    print("positive")
+elif a<0:
+    print("negative")
+else:
+    print("zero") 
---
+++ prime.py
@@ -0,0 +1,9 @@
+# check whether the number is prime or not
+
+a=int(input("enter a number : "))
+for i in range(2,a):
+    if a%i==0:
+        print("not prime")
+        break
+else:
+    print("prime")
